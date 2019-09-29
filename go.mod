33de253f11d52e3a920d242ee07ad9cf22125ce9
u03013112@hotmail.com <u03013112@hotmail.com>
2019-09-29 15:55:33 +0800

    添加test

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/u03013112/ss-config
+
+go 1.12
+
+require (
+	github.com/u03013112/ss-pb v1.0.5
+	golang.org/x/net v0.0.0-20190926025831-c00fd9afed17 // indirect
+	google.golang.org/grpc v1.24.0
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/u03013112/ss-pb/config"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50001"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) Test(ctx context.Context, in *pb.TestRequest) (*pb.TestReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.TestReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterSSConfigServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
